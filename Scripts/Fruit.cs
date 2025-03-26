c35e24a0fa63fd6564509438486b7214813a9927
udesh3128 <ghodadraudesh.2024@gmail.com>
2025-03-26 17:51:46 +0530

    Fruit Slash

--- Scripts/Fruit.cs
+++ Scripts/Fruit.cs
@@ -41,7 +41,7 @@ public class Fruit : MonoBehaviour
         Rigidbody[] slices = sliced.GetComponentsInChildren<Rigidbody>();
         foreach (Rigidbody slice in slices)
         {
-            slice.linearVelocity = fruitRb.linearVelocity;
+            slice.velocity = fruitRb.velocity;
             slice.AddForceAtPosition(direction * force, position, ForceMode.Impulse);
         }
     }
